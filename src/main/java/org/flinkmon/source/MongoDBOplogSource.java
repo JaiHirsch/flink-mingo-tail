f0afc6f3f92ae9a4a160234afd0f0aee636ca749
jaihirsch <jai.hirsch@gmail.com>
2016-05-02 15:59:24 -0500

    Clean up on MongoDBOplogSource

--- src/main/java/org/flinkmon/source/MongoDBOplogSource.java
+++ src/main/java/org/flinkmon/source/MongoDBOplogSource.java
@@ -58,12 +58,18 @@ import com.mongodb.reactivestreams.client.FindPublisher;
 
 public class MongoDBOplogSource extends RichSourceFunction<Document> {
 
+   private static final String OPLOG_OPERATION = "op";
+   private static final String OPLOG_OPERATION_NO_OP = "n";
+   private static final String OPLOG_NAME_SPACE = "ns";
+   private static final String OPLOG_TIMESTAMP = "ts";
+   private static final String OPLOG_ID = "h";
    private static final long serialVersionUID = 1140284841495470127L;
    private volatile boolean isRunning = true;
    private BlockingQueue<Document> opsQueue = new ArrayBlockingQueue<Document>(128);
    ConcurrentMap<Long, AtomicInteger> documentCounter = new ConcurrentHashMap<Long, AtomicInteger>();
    private final String host;
    private final int port;
+   private Integer replicaDepth;
 
    public MongoDBOplogSource(String host, int port) {
       this.host = host;
@@ -90,13 +96,13 @@ public class MongoDBOplogSource extends RichSourceFunction<Document> {
 
    }
 
-   private void bindPublisherToObservable(Entry<String, FindPublisher<Document>> publisher,
+   private void bindPublisherToObservable(Entry<String, FindPublisher<Document>> oplogPublisher,
          ExecutorService executor, MongoCollection<Document> tsCollection) {
-      RxReactiveStreams.toObservable(publisher.getValue()).subscribeOn(Schedulers.from(executor))
-            .subscribe(t -> {
+      RxReactiveStreams.toObservable(oplogPublisher.getValue())
+            .subscribeOn(Schedulers.from(executor)).subscribe(t -> {
                try {
 
-                  putOperationOnOpsQueue(publisher, tsCollection, t);
+                  putOperationOnOpsQueue(oplogPublisher, tsCollection, t);
 
                } catch (InterruptedException e) {
                   e.printStackTrace();
@@ -106,19 +112,25 @@ public class MongoDBOplogSource extends RichSourceFunction<Document> {
 
    private void putOperationOnOpsQueue(Entry<String, FindPublisher<Document>> publisher,
          MongoCollection<Document> tsCollection, Document t) throws InterruptedException {
-      Long opKey = t.getLong("h");
-      BsonTimestamp lastTimeStamp = t.get("ts", BsonTimestamp.class);
-      documentCounter.putIfAbsent(opKey, new AtomicInteger(0));
-      int opCounter = documentCounter.get(opKey).getAndIncrement();
-      String host = publisher.getKey();
-      tsCollection.replaceOne(new Document("host", host),
-            new Document("host", host).append("ts", lastTimeStamp),
-            (new UpdateOptions()).upsert(true));
-      if (opCounter == 2) {
+      updateHostOperationTimeStamp(tsCollection, t.get(OPLOG_TIMESTAMP, BsonTimestamp.class),
+            publisher.getKey());
+      putOperationOnOpsQueueIfFullyReplicated(t);
+   }
 
+   private void putOperationOnOpsQueueIfFullyReplicated(Document t) throws InterruptedException {
+      Long opKey = t.getLong(OPLOG_ID);
+      documentCounter.putIfAbsent(opKey, new AtomicInteger(1));
+      if (documentCounter.get(opKey).getAndIncrement() >= replicaDepth) {
          opsQueue.put(t);
          documentCounter.remove(opKey);
       }
+   }
+
+   private void updateHostOperationTimeStamp(MongoCollection<Document> tsCollection,
+         BsonTimestamp lastTimeStamp, String host) {
+      tsCollection.replaceOne(new Document("host", host),
+            new Document("host", host).append(OPLOG_TIMESTAMP, lastTimeStamp),
+            (new UpdateOptions()).upsert(true));
    }
 
    private Map<String, FindPublisher<Document>> establishMongoPublishers(
@@ -127,6 +139,7 @@ public class MongoDBOplogSource extends RichSourceFunction<Document> {
       try (MongoClient mongoS = new MongoClient(host, port)) {
          for (List<MongoClientWrapper> clients : new ShardSetFinder().findShardSets(mongoS)
                .values()) {
+            if (null == replicaDepth) replicaDepth = clients.size();
             bindHostToPublisher(tsCollection, publishers, clients);
          }
       }
@@ -136,10 +149,10 @@ public class MongoDBOplogSource extends RichSourceFunction<Document> {
    private void bindHostToPublisher(MongoCollection<Document> tsCollection,
          Map<String, FindPublisher<Document>> publishers, List<MongoClientWrapper> clients) {
       for (MongoClientWrapper client : clients) {
-         FindPublisher<Document> publisher = client.getClient().getDatabase("local")
+         FindPublisher<Document> oplogPublisher = client.getClient().getDatabase("local")
                .getCollection("oplog.rs").find().filter(getQueryFilter(tsCollection, client))
                .sort(new Document("$natural", 1)).cursorType(CursorType.TailableAwait);
-         publishers.put(client.getHost(), publisher);
+         publishers.put(client.getHost(), oplogPublisher);
       }
    }
 
@@ -147,16 +160,16 @@ public class MongoDBOplogSource extends RichSourceFunction<Document> {
          MongoClientWrapper client) {
       Document lastTimeStamp = tsCollection.find(new Document("host", client.getHost())).limit(1)
             .first();
-      return getTimeQuery(lastTimeStamp == null ? null : (BsonTimestamp) lastTimeStamp.get("ts"));
+      return getTimeQuery(lastTimeStamp == null ? null : (BsonTimestamp) lastTimeStamp.get(OPLOG_TIMESTAMP));
    }
 
    private Bson getQueryFilter(MongoCollection<Document> tsCollection, MongoClientWrapper client) {
-      return and(ne("ns", "time_d.repl_time"), ne("op", "n"), exists("fromMigrate", false),
+      return and(ne(OPLOG_NAME_SPACE, "time_d.repl_time"), ne(OPLOG_OPERATION, OPLOG_OPERATION_NO_OP), exists("fromMigrate", false),
             getFilterLastTimeStamp(tsCollection, client));
    }
 
    private Bson getTimeQuery(BsonTimestamp lastTimeStamp) {
-      return lastTimeStamp == null ? new Document() : gt("ts", lastTimeStamp);
+      return lastTimeStamp == null ? new Document() : gt(OPLOG_TIMESTAMP, lastTimeStamp);
    }
 
    @Override
